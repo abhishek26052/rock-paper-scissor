02b0bdc980279226928d58d2699063fc17645f67
Abhishek <anbver810@gmail.com>
2025-11-20 21:34:02 +0530

    Javascript Added

---
+++ script.js
@@ -0,0 +1,81 @@
+const userInput = document.body.querySelector("#userDiv");
+const compInput = document.body.querySelector("#compDiv");
+let userChoice;
+let compChoice;
+
+
+document.body.querySelectorAll(".btn").forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        if (e.target.id === "Stone") {
+            userInput.innerHTML = ""
+            userChoice ="Stone";
+            const Stone = `<img class="h-20 w-[132px]" src="./src/Assets/stone.webp" alt="Stone">`
+            userInput.innerHTML += Stone;
+            randomImageputter();
+        }
+        else if (e.target.id === "Paper") {  
+            userInput.innerHTML = ""
+            userChoice ="Paper";
+            const Paper = ` <img class="w-33" src="./src/Assets/paper.webp " alt="Paper">`
+            userInput.innerHTML += Paper;
+            randomImageputter();
+        }
+        else if (e.target.id === "Scissor") {
+            userInput.innerHTML = ""
+            userChoice ="Scissor";
+            const Scissor = `<img class="w-33" src="./src/Assets/Scissor.webp" alt="Scissor">`
+            userInput.innerHTML += Scissor;
+            randomImageputter();
+        }
+
+    })
+})
+
+function randomImageputter() {
+    randomValue = Math.floor(Math.random() * 10)
+    if (randomValue <= 3) {
+        
+        compInput.innerHTML = ""
+        const Stone = `<img class="h-20 w-[132px]" src="./src/Assets/stone.webp" alt="Stone">`
+        compInput.innerHTML += Stone;
+        compChoice="Stone"
+      
+    }
+    else if (randomValue > 3 && randomValue <= 6) {
+        
+        compInput.innerHTML = ""
+        const Paper = ` <img class="w-33" src="./src/Assets/paper.webp " alt="Paper">`
+        compInput.innerHTML += Paper;
+        compChoice="Paper"
+        
+    }
+    else if (randomValue > 6 && randomValue <= 9) {
+        compInput.innerHTML = ""
+        const Scissor = `<img class="w-33" src="./src/Assets/Scissor.webp" alt="Scissor">`
+        compInput.innerHTML += Scissor;
+        compChoice="Scissor"
+    }
+
+    gameResult();
+}
+
+function gameResult() {
+    const rules ={
+        Stone: "Scissor",
+        Paper:"Stone",
+        Scissor:"Paper"
+    }
+    if(userChoice === compChoice){
+       alert("draw");  
+    }
+    else if(rules[userChoice]===compChoice){
+       alert("user wins");  
+    }
+    else{
+       alert("comp wins");
+    }
+
+    
+}
+
+
